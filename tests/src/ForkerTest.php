9677dc42c6615a267c3049d7ccef599140319097
Matt Light <matt.light@lightdatasys.com>
2018-02-06 23:47:09 -0800

    Setup testing boilerplate

---
+++ tests/src/ForkerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lstr\Tines;
+
+use PHPUnit_Framework_TestCase;
+
+class ForkerTest extends PHPUnit_Framework_TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
