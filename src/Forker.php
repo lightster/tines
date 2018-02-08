c59c8ece17cf76f750b72ec6e4f8d23162f3a160
Matt Light <matt.light@lightdatasys.com>
2018-02-07 23:59:16 -0800

    Update fork to accept an array of callbacks

--- src/Forker.php
+++ src/Forker.php
@@ -6,23 +6,23 @@ use Exception;
 
 class Forker
 {
-    public function fork(callable $fork_callback, $fork_count, $data)
+    public function fork(array $callbacks)
     {
         $pids = [];
 
-        for ($i = 1; $i <= $fork_count; $i++) {
+        foreach ($callbacks as $callback_name => $callback) {
             $pid = pcntl_fork();
 
             if (-1 == $pid) {
-                throw new Exception("Could not create fork #{$i}.");
+                throw new Exception("Could not create fork #{$callback_name}.");
             }
 
             if (!$pid) {
-                $exit_status = (int)call_user_func($fork_callback, $i, $data);
+                $exit_status = (int)call_user_func($callback, $callback_name);
                 exit($exit_status);
             }
 
-            $pids[$pid] = $i;
+            $pids[$pid] = $callback_name;
         }
 
         $exit_statuses = [];
@@ -40,24 +40,5 @@ class Forker
         }
 
         return $exit_statuses;
-    }
-
-    public function forkRows(callable $fork_callback, $fork_rows)
-    {
-        $keys = array_keys($fork_rows);
-
-        $exit_statuses = $this->fork(function ($i, $data) use ($fork_callback) {
-            $key = $data['keys'][$i - 1];
-            $row = $data['rows'][$key];
-
-            return call_user_func($fork_callback, $key, $row);
-        }, count($fork_rows), ['keys' => $keys, 'rows' => $fork_rows]);
-
-        $row_exit_statuses = [];
-        foreach ($exit_statuses as $i => $exit_status) {
-            $row_exit_statuses[$keys[$i - 1]] = $exit_status;
-        }
-
-        return $row_exit_statuses;
     }
 }
--- tests/src/ForkerTest.php
+++ tests/src/ForkerTest.php
@@ -8,6 +8,27 @@ class ForkerTest extends PHPUnit_Framework_TestCase
 {
     public function testTrue()
     {
-        $this->assertTrue(true);
+        $forker = new Forker();
+
+        $exit_statuses = $forker->fork([
+            'zero' => function() {
+                return 0;
+            },
+            'two' => function() {
+                return 2;
+            },
+            'three' => function() {
+                return 3;
+            },
+        ]);
+
+        $this->assertEquals(
+            [
+                'zero'  => 0,
+                'two'   => 2,
+                'three' => 3,
+            ],
+            $exit_statuses
+        );
     }
 }
